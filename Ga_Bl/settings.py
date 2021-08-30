9ae7066e8f3a78a3b73ba8418c942525897685c5
Kshitij <kshitijbudholiya2006@gmail.com>
2021-08-30 11:20:50 +0530

    fixed

--- Ga_Bl/settings.py
+++ Ga_Bl/settings.py
@@ -131,13 +131,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#HTTPS Settings
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
-
-#HSTS Settings
-SECURE_HSTS_SECONDS = 31536000 #year
-SECURE_HSTS_PRELOAD = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
