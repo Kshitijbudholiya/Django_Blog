7d78b3bb7cdfdf7fedc4e9fd1c4b5da01201c03c
Kshitij <kshitijbudholiya2006@gmail.com>
2021-09-03 12:37:00 +0530

    Hii

--- Ga_Bl/settings.py
+++ Ga_Bl/settings.py
@@ -25,7 +25,7 @@ with open(BASE_DIR / 'MyNameIs.txt') as f:
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
